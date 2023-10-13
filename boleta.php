4322bcc82aafd2c9498688f9edc69279b25d5a50
iAmRatix <pccarrillop@gmail.com>
2023-10-12 22:44:51 -0500

    ddd

--- boleta.php
+++ boleta.php
@@ -7,7 +7,12 @@ if (isset($_POST['subir'])) {
         $tamano = $_FILES['archivo']['size'];
         $temp = $_FILES['archivo']['tmp_name'];
         
-        
+        $permitted_chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $nuevonombre = substr(str_shuffle($permitted_chars), 0, 16);
+
+
+
+
         $ruta_nuevo_destino = $_ENV['RAILWAY_VOLUME_MOUNT_PATH'].'/' . $_FILES['archivo']['name'];
         $extensiones = array(0=>'image/jpg',1=>'image/jpeg',2=>'image/png');
         if ( in_array($_FILES['archivo']['type'], $extensiones) ) {
@@ -15,11 +20,13 @@ if (isset($_POST['subir'])) {
              if ( $_FILES['archivo']['size']< $max_tamanyo ) {
                   if( move_uploaded_file ( $temp, $ruta_nuevo_destino ) ) {
                   
-                    echo '<meta http-equiv="refresh" content="0;url=https://www.practirent.com">';
 
+                    echo $tipo;
+                    echo $nuevonombre;
+                    echo $nuevonombre.'.'.$tipo;
+                    
                   }else{
-                    echo '<meta http-equiv="refresh" content="0;url=https://www.google.com">';
-
+                    
                   }
              }
         }
